edf10455af8250f33e0c1d89a63d24610592f03d
Michal Pasierbski <mpasierbski@gmail.com>
2023-08-12 19:09:28 +0200

    Cleanup navigation

--- src/navigation.js
+++ src/navigation.js
@@ -1,4 +1,4 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
@@ -10,152 +10,18 @@ export const headerData = {
       text: 'About us',
       href: getPermalink('/about'),
     },
-    // {
-    //   text: 'Landing',
-    //   links: [
-    //     {
-    //       text: 'Sass',
-    //       href: getPermalink('/landing/saas'),
-    //     },
-    //     {
-    //       text: 'Startup',
-    //       href: getPermalink('/landing/startup'),
-    //     },
-    //     {
-    //       text: 'Mobile App',
-    //       href: getPermalink('/landing/mobile-app'),
-    //     },
-    //   ],
-    // },
-    // {
-    //   text: 'Pages',
-    //   links: [
-    //     {
-    //       text: 'Features (Anchor Link)',
-    //       href: getPermalink('/#features'),
-    //     },
-    //     {
-    //       text: 'Services',
-    //       href: getPermalink('/services'),
-    //     },
-    //     {
-    //       text: 'Pricing',
-    //       href: getPermalink('/pricing'),
-    //     },
-    //     {
-    //       text: 'About us',
-    //       href: getPermalink('/about'),
-    //     },
-    //     {
-    //       text: 'Contact',
-    //       href: getPermalink('/contact'),
-    //     },
-    //     {
-    //       text: 'Terms',
-    //       href: getPermalink('/terms'),
-    //     },
-    //     {
-    //       text: 'Privacy policy',
-    //       href: getPermalink('/privacy'),
-    //     },
-    //   ],
-    // },
-    //   {
-    //     text: 'Blog',
-    //     links: [
-    //       {
-    //         text: 'Blog List',
-    //         href: getBlogPermalink(),
-    //       },
-    //       {
-    //         text: 'Article',
-    //         href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-    //       },
-    //       {
-    //         text: 'Article (with MDX)',
-    //         href: getPermalink('markdown-elements-demo-post', 'post'),
-    //       },
-    //       {
-    //         text: 'Category Page',
-    //         href: getPermalink('tutorials', 'category'),
-    //       },
-    //       {
-    //         text: 'Tag Page',
-    //         href: getPermalink('astro', 'tag'),
-    //       },
-    //     ],
-    //   },
-    //   {
-    //     text: 'Widgets',
-    //     href: '#',
-    //   },
-    //   {
-    //     text: 'Contact',
-    //     href: '#',
-    //   },
   ],
-  actions: [
-    // { type: 'button', text: 'Download', href: 'https://github.com/onwidget/astrowind' },
-  ],
+  actions: [],
 };
 
 export const footerData = {
-  links: [
-    // {
-    //   title: 'Product',
-    //   links: [
-    //     { text: 'Features', href: '#' },
-    //     { text: 'Security', href: '#' },
-    //     { text: 'Team', href: '#' },
-    //     { text: 'Enterprise', href: '#' },
-    //     { text: 'Customer stories', href: '#' },
-    //     { text: 'Pricing', href: '#' },
-    //     { text: 'Resources', href: '#' },
-    //   ],
-    // },
-    // {
-    //   title: 'Platform',
-    //   links: [
-    //     { text: 'Developer API', href: '#' },
-    //     { text: 'Partners', href: '#' },
-    //     { text: 'Atom', href: '#' },
-    //     { text: 'Electron', href: '#' },
-    //     { text: 'AstroWind Desktop', href: '#' },
-    //   ],
-    // },
-    // {
-    //   title: 'Support',
-    //   links: [
-    //     { text: 'Docs', href: '#' },
-    //     { text: 'Community Forum', href: '#' },
-    //     { text: 'Professional Services', href: '#' },
-    //     { text: 'Skills', href: '#' },
-    //     { text: 'Status', href: '#' },
-    //   ],
-    // },
-    // {
-    //   title: 'Company',
-    //   links: [
-    //     { text: 'About', href: '#' },
-    //     { text: 'Blog', href: '#' },
-    //     { text: 'Careers', href: '#' },
-    //     { text: 'Press', href: '#' },
-    //     { text: 'Inclusion', href: '#' },
-    //     { text: 'Social Impact', href: '#' },
-    //     { text: 'Shop', href: '#' },
-    //   ],
-    // },
-  ],
+  links: [],
   secondaryLinks: [
     { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    // { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: '#' },
-    // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    // { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/pasierb/funwithkids-website' },
   ],
   footNote: `
     Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://www.linkedin.com/in/mpasierbski/"> Michal Pasierbski</a> · All rights reserved.
