092df6c633eacbd989cc6a3b5c6f3de4b1e9cc47
Michal Pasierbski <mpasierbski@gmail.com>
2023-08-18 21:48:55 +0200

    Re-add Spot interface

--- src/stores/spots.store.ts
+++ src/stores/spots.store.ts
@@ -1,5 +1,14 @@
 import { atom } from 'nanostores';
-import type { Spot } from '~/services/spotsService';
+
+interface Spot {
+  created_at: string;
+  has_restaurant: boolean;
+  has_toilet: boolean;
+  id: number;
+  lat: number;
+  lon: number;
+  name: string;
+}
 
 export const $spots = atom<Spot[]>([]);
 export const $selectedSpot = atom<Spot | null>(null);
